513cd91c66c20efe5db74df9aa5eae68973e3fed
Peter Guthrie <peterbguthrie@gmail.com>
2020-07-16 12:47:33 -0400

    added btn to see code after running it

---
+++ js/TacoCatCode.js
@@ -0,0 +1,34 @@
+﻿$("#btnTest").click(function () {
+    let inputWord = $("#pal").val()
+
+    // Test length
+    if (inputWord.length < 2) {
+        // Request user us a longer character set
+        $("#title").text("Attention!");
+        $("#sub").text("Please use two or more characters.");
+    } else {
+        let word = inputWord.replace(/\s|\'|\"/g, '').toLowerCase();
+
+        let reversedInput = inputWord.split('').reverse().join('');
+
+        let revword = reversedInput.replace(/\s|\'|\"/g, '').toLowerCase();
+
+        let message = `Your input was: ${inputWord}. Reversed is ${reversedInput}.`;
+        let result = ""
+
+        if (word == revword) {
+            result = "Congratulations";
+        } else {
+            result = "Unfortunately";
+        };
+        $("#title").text(result);
+        $("#sub").text(message);
+    }
+});
+
+// Clear page
+$("#btnclear").click(function () {
+    $("#pal").val("");
+    $("#title, #sub").text("");
+
+});
